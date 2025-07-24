4fddb35bb3f6d3294e87de83085a13c90f794096
DaddyBane <theadv1@proton.me>
2025-07-25 01:30:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -58,4 +58,5 @@ def submit():
 
 if __name__ == '__main__':
     init_db()
-    app.run(host='0.0.0.0', port=3000)
+    port = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=port)
